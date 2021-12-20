c4e8575627a5d2d85282a9b347980f7d51ac1218
yunwkgus <yunwkgus@naver.com>
2021-12-20 12:16:21 +0900

    Initial Commit

---
+++ SnoozeIsland/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnoozeIsland
+//
+//  Created by 윤자현 on 2021/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnoozeIsland/SnoozeIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozeIslandApp.swift
+//  SnoozeIsland
+//
+//  Created by 윤자현 on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SnoozeIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
